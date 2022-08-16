bbd609b71a464a04e2f712bc211109522544de8e
Amranabdi1 <amranabdikadir101@gmail.c0m>
2022-08-16 23:33:31 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = 'Asia Abdi';
+const welcomeMessage = 'Welcome to Flatbook, Asia Abdi!';
+const excitedWelcomeMessage =welcomeMessage.toUpperCase();
+
+const shortGreeting = 'Welcome, A!';
